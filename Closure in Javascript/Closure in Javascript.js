ca25a48fc7e1de6e6a9338362b833789a75e81ea
Mohammodrubel <74967048+mohammodrubel@users.noreply.github.com>
2021-08-02 17:42:15 +0600

    Add files via upload

---
+++ Closure in Javascript/Closure in Javascript.js
@@ -0,0 +1,35 @@
+
+function test (){
+    var msg = 'Hello i am biginer programmer'
+        function second(){
+            console.log(msg)
+        }
+        second ()
+
+}
+test()
+
+
+ closure is when a function is able to remember and access 
+ it's lexical scope even when that function executing outside it's lexical scope
+function test (){
+    var msg = 'Hello i am biginer programmer'
+    return function(){
+        console.log(msg)
+    }
+}
+var result =test()
+result ()
+function main (input){
+    console.log(input)
+}
+
+function maltiply(n){
+    return n * 5
+}
+
+function sum (a,b){
+    return a + b
+}
+
+main(maltiply(sum(10,20)))
